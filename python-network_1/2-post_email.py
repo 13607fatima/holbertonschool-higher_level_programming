71d60199bc63f44b90587795690183397529b29b
13607fatima <13607@holbertonstudents.com>
2026-04-25 14:51:00 +0000

    throw awat

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -9,14 +9,11 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    
+    email = sys.argv[2]    
     values = {'email': email}
     data = urllib.parse.urlencode(values).encode('utf-8')
-    
     headers = {'cfclearance': 'true'}
     req = urllib.request.Request(url, data=data, headers=headers)
-
     with urllib.request.urlopen(req) as response:
         body = response.read()
         print(body.decode('utf-8'))
